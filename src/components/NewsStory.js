6d3629028ea286f0cd7f95d88877396dfa98005f
Cameo Langford <cameo.langford@myob.com>
2018-11-21 10:20:31 +1100

    Fixed https/http issues blocking propeller

--- src/components/NewsStory.js
+++ src/components/NewsStory.js
@@ -9,9 +9,6 @@ class NewsStory extends Component {
                 <div className="pmd-card pmd-card-default pmd-z-depth">
                     <a href={story.url || story.link} className="card-link">
                     <div className="pmd-card-title">
-                        {/* <div className="media-left">
-                                <img className="avatar-list-img" width="40" height="40" src={story.thumbnail ? story.thumbnail : 'http://propeller.in/assets/images/profile-pic.png'} alt={story.title}/>
-                        </div> */}
                         <div className="media-body media-middle">
                             <h6 className="pmd-card-title-text">{story.title}</h6>
                             <span className="pmd-card-subtitle-text">{story.source}</span>
